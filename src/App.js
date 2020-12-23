9f00b664b033937986913e5e595dd9b2940e11ff
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-12-23 21:29:24 +0200

    starting image login form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./home";
 import Check from "./check";
+import Login from "./components/login/Login";
 
 function App() {
   const image = "hello.jpg";
   return (
     <div className="App">
-      <Home image />
+      {/* <Home image />
       <Check image />
+       */}
+      <Login />
     </div>
   );
 }
---
+++ src/components/login/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import waxe from "../../assets/img/waxe.png";
+import pro from "../../assets/img/pro.png";
+
+const Login = () => {
+  return (
+    <div>
+      <img alt="this is it" src={waxe} />
+      <div>
+        <img src={pro} alt="profile"></img>
+      </div>
+      <div className={login - container}>
+        <form>
+          <img src={waxe} />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
